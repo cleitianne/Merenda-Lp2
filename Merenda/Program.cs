a49716542e1501ee5f9024736ad98820e4bd5a37
jwellington58 <wj070996@gmail.com>
2019-06-05 14:11:26 -0300

    url de deploy

--- Merenda/Program.cs
+++ Merenda/Program.cs
@@ -26,7 +26,7 @@ namespace Merenda
 
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://*:8082") // Porta
+                .UseUrls("http://*:8081") // Porta
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .UseConfiguration(config)
